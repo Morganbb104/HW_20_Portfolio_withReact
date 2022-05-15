2c35e7e9a27ff1fe577849dbb6372d032893392d
CHENG CHIEN <hcc780921@gmail.com>
2022-05-15 16:41:55 -0700

    App.js added

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
