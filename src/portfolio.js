f28304a8cc9965e9857a2c2cb7d6d8633057e478
Chantel Carter <chantel.alexia@gmail.com>
2024-03-25 14:14:59 -0700

    updated projects pictures with styling

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,5 +1,6 @@
 // import aboutpic from "./components/Access/mePhoto.jpg"
-import test from "./assets/test.jpg"
+import catTinder from "./assets/cat-tinder.png"
+import perfume from "./assets/coming-soon.png"
 
 const header = {
   // all the properties are optional - can be left empty or deleted
@@ -23,45 +24,22 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Movie Tracker',
-    image: test,
+    name: 'Cat Tinder',
+    image: catTinder,
     description:
-      'As users, we see a page that lets anyone look for the movies from the search bar by key words, and obtaining the actual or similar movies with their years, actors, rating, and description information by recommendation. In addition, they can search the nearby theaters from where they currently are located.',
-    stack: ['localstorage', 'Openweather API', 'Google Map API','Movie Database API'],
-    sourceCode: 'https://github.com/Mike-C-Sanders/Movie-Tracker',
-    livePreview: 'https://mike-c-sanders.github.io/Movie-Tracker/',
+      "Cat Tinder. It's like Tinder, but for cats. Cat Tinder is a full-stack, decoupled application that creates profiles for cats and allows a user to see other cat profiles. A decoupled application means we are actually creating two separate applications. A React application to create the views in the frontend and a Rails API that handles our data.",
+    stack: ['React.js', 'Ruby on Rails', 'PostgreSQL'],
+    sourceCode: 'https://github.com/chantelcarter/cat-tinder-frontend-cc',
+    livePreview: 'https://cat-tinder-frontend-wotv.onrender.com',
   },
   {
-    name: 'Event Planner',
+    name: 'Perfume App',
+    image: perfume,
     description:
-      'Event Planner was designed with the partier in mind, the one who wants to find fine dinning or a friend to have a good time. Event Planner is the easiest way for anyone to find an event to attend, create or host your own event or browse whats happening in your local area.',
-    stack: ['swiper.js', 'npm', 'jawsdb','handlebars','express','nodejs'],
-    sourceCode: 'https://github.com/Morganbb104/Event_Planner_project2',
-    livePreview: 'https://obscure-chamber-98928.herokuapp.com/',
-  },
-  {
-    name: 'Resource Planner',
-    description:
-      'Resource Planner is able to distrute the current resource including human resource,time and budget in one company in order to help user to manahe their projects',
-    stack: ['html-css-javascript', 'sequelize', 'React'],
-    sourceCode: 'https://github.com/stone-com/Resource-Planner',
-    livePreview: 'https://resource-planner-group2.herokuapp.com/',
-  },
-  {
-    name: 'GameStoreData(back end)',
-    description:
-      'GameStoreData is able to let user retrieve the game from database based on its year and category',
-    stack: ['Java 8', 'MySQL', 'React'],
-    sourceCode: 'https://github.com/stone-com/Resource-Planner',
-    
-  },
-  {
-    name: 'Music Store(back end)',
-    description:
-      'MusicStore is able to direct user to the desired page based on its endpoints by track, album,artist and label',
-    stack: ['html-css-javascript', 'sequelize', 'React'],
-    sourceCode: 'https://github.com/Morganbb104/M7-project2-musictore-recommendations-Huang_Chengchien',
-    livePreview: 'https://cheng32-musicstore-catalog.herokuapp.com/track',
+      'A full-stack web application with complete CRUD actions. Sign up to add to our perfume collection and discover the notes of different fragrances with plenty more features to come!',
+    stack: ['React.js', 'Ruby on Rails', 'PostgreSQL'],
+    sourceCode: 'https://github.com/chantelcarter/perfume-app-frontend',
+    livePreview: '',
   },
 ]
 
